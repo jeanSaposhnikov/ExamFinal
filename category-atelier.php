7fa7463f2f561b2e5fe5f034922dcce09b05c3e8
Saposhnikov <jean.saposhnikov@hotmail.fr>
2020-03-09 11:46:29 -0400

    question-4

--- category-atelier.php
+++ category-atelier.php
@@ -8,16 +8,29 @@
  */
 
 get_header();
+
+			echo "<h2>". category_description( get_category_by_slug('atelier')) . "</h2>";
 ?>
-        
 			<section id="ateliers">	
 			<?php
-			echo "<h2>". category_description( get_category_by_slug('atelier')) . "</h2>"; 
+			 
 			$article=1;
 			while ( have_posts() ) :
 				 the_post();
-				 echo '<p>'. $article . ". ".get_the_title(). "____".get_post_field('post_name')."______".	get_the_author_meta( 'display_name', $post->post_author )  . '</p>';
-				 $article++;
+				 /*
+				 	echo '<p>'. $article . ". ".get_the_title(). "____".get_post_field('post_name')."______".	get_the_author_meta( 'display_name', $post->post_author )  . '</p>';
+				 	$article++;
+				 */
+
+			
+				$heure = substr(get_post_field('post_name'), 4);
+				//echo $heure;
+				$prof=	get_the_author_meta( 'display_name', $post->post_author );
+	
+				$gridArea = $heure . '/' . $prof . '/' . ($heure+1) . '/' . ($prof+1);
+				echo '<p>' .$gridArea . '</p>' ;
+				echo '<h2  style="grid-area:'. $gridArea . '">' . get_the_title() . $gridArea .get_post_field('post_name'). get_the_author_meta( 'display_name', $post->post_author )  .'</h2>';
+
 			endwhile;
 		?>
         </section>
